90af8a7aec7479b28ede76356612202531f314da
psypig99 <psypig99@gmail.com>
2020-08-28 09:58:40 +0900

    properties test

--- src/test/java/com/template/springboot/jpaTest/controller/TestJpaRestControllerTest.java
+++ src/test/java/com/template/springboot/jpaTest/controller/TestJpaRestControllerTest.java
@@ -1,32 +1,34 @@
 package com.template.springboot.jpaTest.controller;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest(
+        properties = {
+                "testId = psypig992",
+                "testName = sejun2"
+        }
+)
+@Transactional
 class TestJpaRestControllerTest {
 
-    @Test
-    void getAllMembers() {
-    }
+    private Logger log = LoggerFactory.getLogger(TestJpaRestControllerTest.class);
+
+    @Value("${testId}")
+    private String testId;
+
+    @Value("${testName}")
+    private String testName;
 
     @Test
-    void getMember() {
+    void getMember() throws Exception{
+        log.info("#### Properteis Test ####");
+        log.info("testId : " + testId);
+        log.info("testName : " + testName);
     }
 
-    @Test
-    void deleteMember() {
-    }
-
-    @Test
-    void updateMember() {
-    }
-
-    @Test
-    void save() {
-    }
-
-    @Test
-    void testSave() {
-    }
 }
